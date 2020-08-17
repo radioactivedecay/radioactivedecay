4972ba0631d1ddad77c2eefa74ff468d03bf80a0
Alex Malins <github@alexmalins.com>
2020-08-17 18:14:08 +0900

    Add support for loading decay data from user defined path

--- radioactivedecay/decaydata.py
+++ radioactivedecay/decaydata.py
@@ -13,18 +13,21 @@ except ImportError:
 class DecayData:
     '''Stores data for radioactive decay calculations'''
     # pylint: disable=too-many-instance-attributes
-    def __init__(self, dataset='icrp107'):
+    def __init__(self, dataset='icrp107', path=None):
         self.dataset = dataset
-        self.load_data()
+        self.load_data(path)
 
     def get_path(self, filename):
         '''Get path of file to open'''
-        with pkg_resources.path(__package__+'.'+self.dataset, filename) as path:
-            return path
+        with pkg_resources.path(__package__+'.'+self.dataset, filename) as package_path:
+            return package_path
 
-    def load_data(self):
+    def load_data(self, path):
         '''Read from files containing nuclear data'''
-        data = np.load(self.get_path('radionuclides_decay_consts.npz'))
+        if path is None:
+            data = np.load(self.get_path('radionuclides_decay_consts.npz'))
+        else:
+            data = np.load(path+'/radionuclides_decay_consts.npz')
         self.nuclide_names = data['nuclide_names']
         self.decay_consts = data['decay_consts']
         self.year_conv = data['year_conv']
@@ -35,8 +38,12 @@ class DecayData:
                                            (np.arange(self.no_nuclides),
                                             np.arange(self.no_nuclides))))
 
-        self.matrix_c = sparse.load_npz(self.get_path('c.npz'))
-        self.matrix_c_inv = sparse.load_npz(self.get_path('cinverse.npz'))
+        if path is None:
+            self.matrix_c = sparse.load_npz(self.get_path('c.npz'))
+            self.matrix_c_inv = sparse.load_npz(self.get_path('cinverse.npz'))
+        else:
+            self.matrix_c = sparse.load_npz(path+'/c.npz')
+            self.matrix_c_inv = sparse.load_npz(path+'/cinverse.npz')
 
     def __repr__(self):
         return 'Decay dataset: '+self.dataset
