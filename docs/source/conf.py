9478a6f6898e25011efe963f80837ce7c66d28d8
Alex Malins <github@alexmalins.com>
2020-12-02 22:03:31 +0900

    Fix release number in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2020, Alex Malins"
 author = "Alex Malins"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.9"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
