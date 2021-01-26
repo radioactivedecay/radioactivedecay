bf00c730a7175e9a96941b71e2eb0e51f7045892
Alex Malins <github@alexmalins.com>
2021-01-26 11:13:26 +0900

    Fix version numbers, updated logo

--- radioactivedecay/__init__.py
+++ radioactivedecay/__init__.py
@@ -10,7 +10,7 @@ mode using SymPy routines which gives more accurate results for decay chains
 with orders of magnitude differences between radionuclide half-lives.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.2.1"
 
 from radioactivedecay.decaydata import DecayData, DEFAULTDATA
 from radioactivedecay.radionuclide import Radionuclide
